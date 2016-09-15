fb837009d3b68e6024a3c4799952bcbd6ed6b490
scriptype <scriptyper@gmail.com>
2016-09-16 01:52:44 +0300

    Generate as many AvatarCanvas items as quantity & use them as items of BlockList

--- src/scripts/components/Content.js
+++ src/scripts/components/Content.js
@@ -1,17 +1,36 @@
 import React from 'react'
 import BlockList from '../modules/BlockList'
+import AvatarCanvas from '../modules/AvatarCanvas'
 
 export default React.createClass({
   propTypes: {
     parameters: React.PropTypes.object.isRequired
   },
 
+  generateItems() {
+    var {
+      quantity,
+      width,
+      height,
+      rotation,
+      colors
+    } = this.props.parameters
+
+    return new Array(quantity)
+      .join(' ')
+      .split(' ')
+      .map(e => (
+          <AvatarCanvas {...{ width, height, rotation, colors }} />
+      ))
+  },
+
   render() {
     console.log('Content.props', this.props)
+    var items = this.generateItems()
     return (
       <div className='content'>
         <BlockList
-          items={[]}
+          items={items}
           className='canvas-list' />
       </div>
     )
